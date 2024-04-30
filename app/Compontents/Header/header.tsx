34eb000b47334cb8cb7937a8adc22a8cdccba0ba
Henrik <staranderh@gmail.com>
2024-04-30 15:14:41 +0200

    load img

--- app/Compontents/Header/header.tsx
+++ app/Compontents/Header/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
       
         <header>
             <div className="container">
-                {/* <img className="me" src="./me.jpg" alt="" /> */}
+                <img className="me" src="./me.jpg" alt="" />
                 <a href="">Home</a>
                 <a href="">About</a>
                 <a href="">Contact</a>
