59c97c6f8985872741a59e481361fa958ca3d2df
Henrik <staranderh@gmail.com>
2024-04-30 15:55:28 +0200

    try

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,29 @@ export default function Home() {
   return (
     <main>
       <section className="home">
-        <div className="left">
-          <nav>
-              <a href="">Home</a>
-              <a href="">About</a>
-              <a href="">Contact</a>
-          </nav>
-        </div>
-        <div className="right">
-          <div className="container">  
-            <h4>Personuppgift: 880218-5119</h4>
-            <p>
-                Studerar till Webbutvecklare inom .NET på EC-Utbildning i Göteborg. Under utbildningen får vi lära dig grunderna 
-                och avancerade aspekter av webbutveckling genom Microsofts .NET-ramverk, programmeringsspråk som C# och 
-                tekniker för att skapa robusta webbapplikationer. Vi utvecklar interaktiva webbsidor, cms system, applikationer och 
-                gränssnitt mot användare. Utbildningen innhåller kunskap både inom frontend och backend.
-                Mer info: <a href="https://ecutbildning.se/utbildningar/webbutvecklare-inom-net/#om-utbildningen">https://ecutbildning.se/utbildningar/webbutvecklare-inom-net/#om-utbildningen</a>
-                Tidigare lång erfarenhet inom el-branshen. Drifttekniker och underhållstekniker.
-            </p>    
+        <div className="container">
+          {/* <div className="left">
+            <nav>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </nav>
+          </div> */}
+          <div className="right">
+            <div className="container">  
+              <h4>Personuppgift: 880218-5119</h4>
+              <p>
+                  Studerar till Webbutvecklare inom .NET på EC-Utbildning i Göteborg. Under utbildningen får vi lära dig grunderna 
+                  och avancerade aspekter av webbutveckling genom Microsofts .NET-ramverk, programmeringsspråk som C# och 
+                  tekniker för att skapa robusta webbapplikationer. Vi utvecklar interaktiva webbsidor, cms system, applikationer och 
+                  gränssnitt mot användare. Utbildningen innhåller kunskap både inom frontend och backend.
+                  Mer info: <a href="https://ecutbildning.se/utbildningar/webbutvecklare-inom-net/#om-utbildningen">https://ecutbildning.se/utbildningar/webbutvecklare-inom-net/#om-utbildningen</a>
+                  Tidigare lång erfarenhet inom el-branshen. Drifttekniker och underhållstekniker.
+              </p>    
+            </div>
           </div>
         </div>
+        
         
       </section>
      
