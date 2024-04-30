0bb02ad4223487bfbe912c799f4fe6315765d59e
Henrik <staranderh@gmail.com>
2024-04-30 15:20:45 +0200

    load img

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <main>
-       <div></div>
+       <div>  
+          <h4>Personuppgift: 880218-5119</h4>
+          <p>
+              Studerar till Webbutvecklare inom .NET på EC-Utbildning i Göteborg. Under utbildningen får vi lära dig grunderna 
+              och avancerade aspekter av webbutveckling genom Microsofts .NET-ramverk, programmeringsspråk som C# och 
+              tekniker för att skapa robusta webbapplikationer. Vi utvecklar interaktiva webbsidor, cms system, applikationer och 
+              gränssnitt mot användare. Utbildningen innhåller kunskap både inom frontend och backend.
+              Mer info: <a href="https://ecutbildning.se/utbildningar/webbutvecklare-inom-net/#om-utbildningen">Link</a>
+              Tidigare lång erfarenhet inom el-branshen. Drifttekniker och underhållstekniker.
+          </p>
+                
+        </div>
     </main>
   );
 }
