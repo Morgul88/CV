bd8aa055cf85380bda2ec5a727db88fc660bd137
Henrik <staranderh@gmail.com>
2024-04-30 14:31:00 +0200

    blabla

---
+++ app/Compontents/Footer/footer.tsx
@@ -0,0 +1,21 @@
+import "./footer.css";
+
+export default function Footer() {
+    return (
+      
+        <footer>
+            <div className="container">
+                <div className="social-media">
+                    <a className="btn-social" href="#"><i className="fa-brands fa-facebook"></i></a>
+                    <a className="btn-social" href="#"><i className="fa-brands fa-x-twitter"></i></a>
+                    <a className="btn-social" href="#"><i className="fa-brands fa-square-instagram"></i></a>
+                    <a className="btn-social" href="#"><i className="fa-brands fa-youtube"></i></a>
+
+                </div>
+                <p>Silicon. All rights reserved. Pulvinar urna condimentum amet tempor</p>
+            </div>
+        </footer>
+    
+    );
+  }
+  
---
+++ app/Compontents/Header/header.tsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+      
+        <header>
+            <div className="container">
+                <img className="me" src="/me.jpg" alt="" />
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </div>
+        </header>
+      
+    );
+  }
+  
